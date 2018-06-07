e94dfc61b97703f1e302ef341dfb00fa263380e3
Timofey Mokhnatkin <timofey@retailnext.net>
2018-06-06 20:50:10 -0700

    MANUAL conflict resolution. Revert "fix a small bug in IsEligibleMining"
    This reverts commit 9795b66ffda7181380ddda1c0ced32eb11b884a3.

--- src/caffe/util/bbox_util.cpp
+++ src/caffe/util/bbox_util.cpp
@@ -719,7 +719,7 @@ int CountNumMatches(const vector<map<int, vector<int> > >& all_match_indices,
 }
 
 inline bool IsEligibleMining(const MiningType mining_type, const int match_idx,
-    const float match_overlap, const float neg_overlap) {
+                             const int match_overlap, const int neg_overlap) {
   if (mining_type == MultiBoxLossParameter_MiningType_MAX_NEGATIVE) {
     return match_idx == -1 && match_overlap < neg_overlap;
   } else if (mining_type == MultiBoxLossParameter_MiningType_HARD_EXAMPLE) {
@@ -877,7 +877,7 @@ void MineHardExamples(const Blob<Dtype>& conf_blob,
         vector<int> nms_indices;
         ApplyNMS(sel_bboxes, sel_loss, nms_threshold, top_k, &nms_indices);
         if (nms_indices.size() < num_sel) {
-          LOG(INFO) << "not enough sample after nms: " << nms_indices.size();
+          LOG(INFO) << "not enough sample after nms.";
         }
         // Pick top example indices after nms.
         num_sel = std::min(static_cast<int>(nms_indices.size()), num_sel);
